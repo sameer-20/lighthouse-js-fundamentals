e0ad06e6041818d967feb2c4f5f322d3ad92aa8a
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-07-05 20:45:14 +0000

    Add more conditional example

--- conditionals.js
+++ conditionals.js
@@ -36,3 +36,23 @@ else
 }
 
 /**************************************************************/
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+   console.log("You are eligible to vote.");
+}
+	
+
+if (temperature < -40 || temperature > 40){
+   console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining){
+   console.log("Leave your umbrella at home!");
+}
+
+/******************************************************************/
+
+
