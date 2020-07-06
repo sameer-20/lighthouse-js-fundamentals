4a1cc62428583b5af5a94c8bb0738ed185cac5b8
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-07-06 21:25:40 +0000

    Function Expressions

---
+++ hello.js
@@ -0,0 +1,19 @@
+const sayHello = function () {
+
+	console.log("Hello, world");
+}
+
+sayHello();
+
+/***********************************************************/
+
+const sayHello1 = function (name) {
+	console.log("Hello, " + name);
+}
+
+sayHello1("Sameer");
+sayHello1("Madhu");
+
+/**********************************************************/
+
+
