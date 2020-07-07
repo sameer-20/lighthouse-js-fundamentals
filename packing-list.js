d37582bdbf48e18c3a1461d1ad0b994f38400929
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-07-07 04:40:50 +0000

    Included while loop

--- packing-list.js
+++ packing-list.js
@@ -5,4 +5,14 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
+/*************************************************************/
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+/************************************************************/
 
